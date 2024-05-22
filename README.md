956678373b627940acf3fb302d856d422dc96844
José Luis Pino <jpino@keysight.com>
2024-05-21 20:20:21 -0700

    Minor updates

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
-# Kern River Boater GPX collections
-Garmin GPX Files Optimized for the Garmin Instinct.  The Garmin Instinct has 15 character name limits.
+# Kern River Boater GPX Collections
+These Garmin GPX files are optimized for the Garmin Instinct, which has a 15-character name limit.
 
-## Instuctions
-Currently, the best way to import this is using a PC web brower and opening https://explore.garmin.com/Map
+## Instructions
+To import these files, follow these steps:
 
-* On your Garmin watch, add the map view for the kayak activity
-* Import each file as a new collection https://explore.garmin.com/Map.  Note, you can import the waypoint file as Tracks or Routes.
-* Select all of the waypoints in each collection to set an icon.  I recommend these icons as they render nicely on the Instinct: ![Garmin Icons](garmin-icons.png)
+* On your Garmin watch, add the map view for the kayak activity.
+* Import each file as a new collection using Garmin Explore. Note that you can import the waypoint file as either Tracks or Routes.
+* Select all the waypoints in each collection to set an icon. I recommend using the icons that render nicely on the Instinct: !Garmin Icons.: ![Garmin Icons](garmin-icons.png)
 
-# Other resources
-Many river (Salt, Grand Canyon, Green, etc.) GPX files are available from https://rivermaps.net/pages/gps-waypoints. 
-I recommend breaking them up into different categories, campsites, milestones, POI, and rapids.  Additionally, the files should be a max of 99 waypoints as that seems to be a Collection size limitation in some Garmin devices.  To assist with this preprocessing, I have included a parseRiverMaps.py script to automate breaking apart the GPX into multiple GPX files.
+# Other Resources
+You can find GPX files for many other rivers (such as Salt, Grand Canyon, Green, etc.) at RiverMaps. I recommend breaking them up into different categories, including campsites, milestones, points of interest (POI), and rapids. Additionally, keep the files to a maximum of 99 waypoints, as some Garmin devices have a collection size limitation. To assist with preprocessing, I’ve included a parseRiverMaps.py script that automates breaking apart the GPX into multiple files.
--- RiverMaps/parseRiverMaps.py
+++ RiverMaps/parseRiverMaps.py
@@ -1,59 +1,102 @@
+# Import necessary libraries
 import gpxpy
 import gpxpy.gpx
 import sys
 from pathlib import Path
 import re
 
+# Define a class to handle writing waypoints to GPX files
 class GarminGpxFileWriter:
-    def __init__(self, gpx_file_name, wayPointType):
-        self.wayPointType = wayPointType
-        self.fileNum = 1
-        self.wayPointIndex = 0
+    def __init__(self, gpx_file_name, waypoint_type):
+        """
+        Initialize the GarminGpxFileWriter object.
+
+        Args:
+            gpx_file_name (str): The name of the GPX file.
+            waypoint_type (str): The type of waypoints to add.
+        """
+        # Store the input parameters
+        self.gpx_file_name = gpx_file_name
+        self.waypoint_type = waypoint_type
+        # Initialize counters and data structures
+        self.file_index = 1
+        self.waypoint_index = 0
         self.gpx = gpxpy.gpx.GPX()
+        # Extract the river name from the file name
         self.riverName = Path(gpx_file_name).stem
         
-    def addWayPoint(self,wayPoint):
-        if ( self.wayPointIndex == 99):
-            self.writeFile()
-        self.wayPointIndex = self.wayPointIndex + 1
-        wayPoint.name = wayPoint.name[2:]
-        wayPoint.extensions.clear()
-        self.gpx.waypoints.append(wayPoint)
+    def add_waypoint(self, waypoint):
+        """
+        Add a waypoint to the GPX file.
+
+        Args:
+            waypoint (gpxpy.gpx.GPXWaypoint): The waypoint to add.
+        """
+        # If we've reached the maximum number of waypoints per file, write the current file
+        if self.waypoint_index == 99:
+            self.write_file()
+        # Increment the waypoint index
+        self.waypoint_index += 1
+        # Modify the waypoint name and clear its extensions
+        waypoint.name = waypoint.name[2:]
+        waypoint.extensions.clear()
+        # Add the waypoint to the GPX object
+        self.gpx.waypoints.append(waypoint)
         
-    def writeFile(self):
-        if (self.wayPointIndex > 0):
-            fileName = self.riverName + ' ' + self.wayPointType + ' ' + str(self.fileNum) + '.gpx'
-            fo = open(fileName , 'w')
-            fo.write(self.gpx.to_xml())
-            fo.close()
-            self.fileNum = self.fileNum + 1
-            self.wayPointIndex = 0
+    def write_file(self):
+        """
+        Writes the GPX data to a file.
+
+        If the waypoint index is greater than 0, it creates a file name based on the river name, waypoint type, and file index.
+        Then, it opens the file in write mode and writes the GPX data to it using the `to_xml()` method of the `gpx` object.
+        After writing the data, it increments the file index, resets the waypoint index to 0, and clears the waypoints list in the `gpx` object.
+        """
+        # If there are waypoints to write, write them to a file
+        if self.waypoint_index > 0:
+            # Construct the file name
+            file_name = self.riverName + ' ' + self.waypoint_type + ' ' + str(self.file_index) + '.gpx'
+            # Open the file and write the GPX data
+            with open(file_name, 'w') as fo:
+                fo.write(self.gpx.to_xml())
+            # Increment the file index and reset the waypoint index and GPX object
+            self.file_index += 1
+            self.waypoint_index = 0
             self.gpx.waypoints.clear()
-        
 
-# Parse river file
+# Get the GPX file name from the command line arguments
 gpx_file_name = sys.argv[1]
-gpx_file = open(gpx_file_name)
+# Open the GPX file
+with open(gpx_file_name) as gpx_file:
+    # Create a GarminGpxFileWriter object for each type of waypoint
+    waypoint_files = {
+        'R-': GarminGpxFileWriter(gpx_file_name, 'Rapids'),
+        'C-': GarminGpxFileWriter(gpx_file_name, 'Campgrounds'),
+        'P-': GarminGpxFileWriter(gpx_file_name, 'POI'),
+        'M-': GarminGpxFileWriter(gpx_file_name, 'Miles Markers')
+    }
 
-waypointFiles = {
-    'R-': GarminGpxFileWriter(gpx_file_name, 'Rapids'), 
-    'C-': GarminGpxFileWriter(gpx_file_name, 'Campgrounds'), 
-    'P-': GarminGpxFileWriter(gpx_file_name, 'POI'), 
-    'M-': GarminGpxFileWriter(gpx_file_name, 'Miles Markers')} 
+    # Parse the GPX file
+    gpx = gpxpy.parse(gpx_file)
 
-gpx = gpxpy.parse(gpx_file)
+    # Loop over each waypoint in the GPX file
+    for waypoint in gpx.waypoints:
+        # Print the waypoint's name and coordinates
+        print(f'waypoint {waypoint.name} -> ({waypoint.latitude},{waypoint.longitude})')
 
-for waypoint in gpx.waypoints:
-    print(f'waypoint {waypoint.name} -> ({waypoint.latitude},{waypoint.longitude})')
-    key = waypoint.name[:2]
-    if key == 'R-' and waypoint.description is not None:
-        rapidClass = re.search("(\(\w*)([0-9]+)(\w*\))",waypoint.description)
-        if rapidClass is not None:
-            waypoint.name = waypoint.name + ' ' + rapidClass.group(2)
-    waypointFiles[key].addWayPoint(waypoint)
+        # Extract the waypoint type from the name
+        key = waypoint.name[:2]
 
-# Write the remaining waypoints before exiting
-waypointFiles['R-'].writeFile() 
-waypointFiles['C-'].writeFile() 
-waypointFiles['P-'].writeFile() 
-waypointFiles['M-'].writeFile() 
+        # If the waypoint is a rapid and has a description, add the rapid class to the name
+        if key == 'R-' and waypoint.description is not None:
+            rapid_classification = re.search(r"(\(\w*)([0-9]+)(\w*\))", waypoint.description)
+            if rapid_classification is not None:
+                waypoint.name += ' ' + rapid_classification.group(2)
+
+        # Add the waypoint to the appropriate file
+        waypoint_files[key].add_waypoint(waypoint)
+
+# Write the remaining waypoints to files
+waypoint_files['R-'].write_file() 
+waypoint_files['C-'].write_file() 
+waypoint_files['P-'].write_file() 
+waypoint_files['M-'].write_file()
